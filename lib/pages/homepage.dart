3a1c252acc238e08f2058afea8e70bd05e431ad0
aditya-10k <katheaditya10@gmail.com>
2024-10-03 00:32:14 +0530

    12th

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -27,74 +27,75 @@ class _HomepageState extends State<Homepage> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.white,
-        // Adding bottom navigation bar with constraints and layout fixes
-        bottomNavigationBar: 
          
+          bottomNavigationBar: 
+               Container(
+                height: 70,
+                padding: EdgeInsets.fromLTRB(10, 0, 10, 10),
+                 child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                   child: GNav(
+                    gap: 5,
+                    tabBackgroundColor: Color.fromARGB(255, 0, 85, 55),
+                    textSize: 12,
+                    activeColor: Colors.white,
+                    color: Colors.grey.shade400,
+                    backgroundColor: Color.fromARGB(255, 0, 112, 72),
+                    padding: EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+                    duration: Duration(milliseconds: 400),
+                    onTabChange: (index) {
+                      setState(() {
+                        curindex = index;
+                      });
+                    },
+                    tabs: [
+                      GButton(
+                        icon: Icons.home_filled,
+                        text: 'Home',
+                        iconSize: 30,
+                        leading: Image.asset(
+                          'lib/workingcomps/imgloc/cup.png',
+                          height: 40,
+                          width: 40,
+                        ),
+                      ),
+                      GButton(
+                        icon: Icons.menu,
+                        text: 'Menu',
+                        iconSize: 30,
+                        leading: Image.asset(
+                          'lib/workingcomps/imgloc/menu.png',
+                          height: 40,
+                          width: 40,
+                        ),
+                      ),
+                      GButton(
+                        icon: Icons.shopping_bag_outlined,
+                        text: 'Cart',
+                        iconSize: 30,
+                        leading: Image.asset(
+                          'lib/workingcomps/imgloc/bag.png',
+                          height: 40,
+                          width: 40,
+                        ),
+                      ),
+                      GButton(
+                        icon: Bootstrap.person_circle,
+                        text: 'Profile',
+                        iconSize: 38,
+                        iconActiveColor: Colors.white,
+                        iconColor: Colors.white,
+                      ),
+                    ],
+                                 ),
+                 ),
+               ),
+         
+       
+            body: pagewidgets[curindex], 
+            
           
-           ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: SizedBox(
-              height: 60, // Explicit height for the bottom navigation bar
-              child: GNav(
-                gap: 5,
-                tabBackgroundColor: Color.fromARGB(255, 0, 85, 55),
-                textSize: 12,
-                activeColor: Colors.white,
-                color: Colors.grey.shade400,
-                backgroundColor: Color.fromARGB(255, 0, 112, 72),
-                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 8),
-                duration: Duration(milliseconds: 400),
-                onTabChange: (index) {
-                  setState(() {
-                    curindex = index;
-                  });
-                },
-                tabs: [
-                  GButton(
-                    icon: Icons.home_filled,
-                    text: 'Home',
-                    iconSize: 30,
-                    leading: Image.asset(
-                      'lib/workingcomps/imgloc/cup.png',
-                      height: 40,
-                    ),
-                  ),
-                  GButton(
-                    icon: Icons.menu,
-                    text: 'Menu',
-                    iconSize: 30,
-                    leading: Image.asset(
-                      'lib/workingcomps/imgloc/menu.png',
-                      height: 40,
-                    ),
-                  ),
-                  GButton(
-                    icon: Icons.shopping_bag_outlined,
-                    text: 'Cart',
-                    iconSize: 30,
-                    leading: Image.asset(
-                      'lib/workingcomps/imgloc/bag.png',
-                      height: 40,
-                    ),
-                  ),
-                  GButton(
-                    icon: Bootstrap.person_circle,
-                    text: 'Profile',
-                    iconSize: 38,
-                  ),
-                ],
-              ),
-            ),
-          ),
         
-        // Properly laying out the body content with a column and expanded widget
-        body: Column(
-          children: [
-            Expanded(
-              child: pagewidgets[curindex], // Displaying the selected page
-            ),
-          ],
-        ),
       ),
     );
   }
