4ec23ae3c944c06f5de8f6789d400b160419ab1f
aditya-10k <katheaditya10@gmail.com>
2024-10-16 23:20:27 +0530

    16th update

--- lib/pages/in-app pages/GenPage.dart
+++ lib/pages/in-app pages/GenPage.dart
@@ -1,8 +1,12 @@
+import 'dart:convert';
+
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:coffee/workingcomps/drinksdata.dart';
+import 'package:coffee/workingcomps/model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:http/http.dart' as http;
 
 class GenPage extends StatefulWidget {
   const GenPage({super.key});
@@ -31,6 +35,22 @@ class _GenPageState extends State<GenPage> {
     }
   }
 
+    List<BeverageModel> drinklist = [];
+
+  Future<List<BeverageModel>> getdrinkapi() async {
+    final response = await http.get(Uri.parse('https://api.sampleapis.com/coffee/hot'));
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      drinklist.clear();
+      for (Map i in data) {
+        drinklist.add(BeverageModel.fromJson(i));
+      }
+      return drinklist;
+    } else {
+      return drinklist;
+    }
+  }
+
   List<String> imgsrcc = [
     "assets/21667-easy-iced-coffee-ddmfs-4x3-0093-7becf3932bd64ed7b594d46c02d0889f.jpg",
     "assets/bagel-recipe.jpg",
@@ -45,95 +65,185 @@ class _GenPageState extends State<GenPage> {
 
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          // Welcome message container
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  width: screenWidth - 20,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Color.fromARGB(255, 0, 112, 72),
-                  ),
-                  padding: EdgeInsets.fromLTRB(10, 20, 10, 20),
-                  child: Text(
-                    user != null && user!.displayName != null
-                        ? "Welcome, ${user!.displayName}!"
-                        : "Welcome Guest!",
-                    style: GoogleFonts.robotoSlab(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 30,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // Welcome message container
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: screenWidth - 20,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Color.fromARGB(255, 0, 112, 72),
+                    ),
+                    padding: EdgeInsets.fromLTRB(10, 20, 10, 20),
+                    child: Text(
+                      user != null && user!.displayName != null
+                          ? "Welcome, ${user!.displayName}!"
+                          : "Welcome Guest!",
+                      style: GoogleFonts.robotoSlab(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30,
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
-          ),
-
-          // Serving text
-          Padding(
-            padding: const EdgeInsets.fromLTRB(20, 5, 20, 10),
-            child: Text(
-              "We serve",
-              textAlign: TextAlign.left,
-              style: GoogleFonts.robotoSlab(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-                fontSize: 25,
-              ),
+              ],
             ),
-          ),
-
-          // Carousel slider
-          Container(
-            height: 200,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(15),
-              child: CarouselSlider(
-                options: CarouselOptions(
-                  height: 200,
-                  viewportFraction: 1.0,
-                  autoPlay: true,
+            SizedBox(height: 30,),
+        
+            // Serving text
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 5, 20, 10),
+              child: Text(
+                "We serve",
+                textAlign: TextAlign.left,
+                style: GoogleFonts.robotoSlab(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
                 ),
-                items: imgsrcc.map((img) {
-                  return Builder(
-                    builder: (BuildContext context) {
-                      return InkWell(
-                        child: Image.asset(
-                          img,
-                          fit: BoxFit.cover,
-                          width: screenWidth - 20,
-                        ),
-                      );
-                    },
-                  );
-                }).toList(),
               ),
             ),
-          ),
-
-           SizedBox(height: 10,),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(20, 5, 20, 10),
-            child: Text(
-              "What would you like?",
-              textAlign: TextAlign.left,
-              style: GoogleFonts.robotoSlab(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-                fontSize: 25,
+            SizedBox(height: 30,),
+        
+            Padding(
+              
+              padding: const EdgeInsets.all(10.0),
+              child: FutureBuilder(
+                future: getdrinkapi(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return Center(
+                      child: CircularProgressIndicator(
+                        color: Colors.white,
+                      ),
+                    );
+                  } else {
+                    drinklist.shuffle();
+                    return Container(
+                      
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Color.fromARGB(255, 0, 112, 72),
+                      ),
+              
+                      child: Container(
+                                    height: 200,
+                                    width: screenWidth- 16,
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(15),
+                                      child: CarouselSlider(
+                      options: CarouselOptions(
+                        height: 200,
+                        viewportFraction: 1.0,
+                        autoPlay: true,
+                      ),
+                      items: drinklist.map((drink) {
+                        return Builder(
+                          builder: (BuildContext context) {
+                            return Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Column(
+                                    children: [
+                                      Text(
+                                                  drink.title!, 
+                                                  style: GoogleFonts.robotoSlab(
+                                                    color: Colors.white,
+                                                    fontWeight: FontWeight.bold,
+                                                    fontSize: 20,
+                                                  )),
+                                                  SizedBox(height: 10,),
+                                                  Container(
+                                                    width: screenWidth/2 - 40,
+                                                    child: Text(
+                                                    drink.description!, 
+                                                    style: GoogleFonts.robotoSlab(
+                                                      color: Colors.white,
+                                                      //fontWeight: FontWeight.bold,
+                                                      fontSize: 10,
+                                                    )
+                                                    ),
+                                                  )
+                                                  ,
+                                    ],
+                                  ),
+                                  ClipRRect(
+                                    borderRadius: BorderRadius.circular(15),
+                                    child: Image.network(
+                                      drink.image! ,
+                                      fit: BoxFit.cover,
+                                      width: screenWidth/2 - 20,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                        );
+                      }).toList(),
+                                      ),
+                                    ),
+                                  ),
+                    ); 
+                  }
+                },
               ),
             ),
-          ),
-          SizedBox(height: 10,),
-          Drinksdata(),
-          
-        ],
+        
+            // Container(
+            //   height: 200,
+            //   child: ClipRRect(
+            //     borderRadius: BorderRadius.circular(15),
+            //     child: CarouselSlider(
+            //       options: CarouselOptions(
+            //         height: 200,
+            //         viewportFraction: 1.0,
+            //         autoPlay: true,
+            //       ),
+            //       items: drinklist.map((drink) {
+            //         return Builder(
+            //           builder: (BuildContext context) {
+            //             return InkWell(
+            //               child: Image.asset(
+            //                 drink.image!,
+            //                 fit: BoxFit.cover,
+            //                 width: screenWidth - 20,
+            //               ),
+            //             );
+            //           },
+            //         );
+            //       }).toList(),
+            //     ),
+            //   ),
+            // ),
+        
+             SizedBox(height: 30,),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 5, 20, 10),
+              child: Text(
+                "What would you like?",
+                textAlign: TextAlign.left,
+                style: GoogleFonts.robotoSlab(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
+                ),
+              ),
+            ),
+            SizedBox(height: 30,),
+            Drinksdata(),
+            
+          ],
+        ),
       ),
     );
   }
--- lib/workingcomps/drinksdata.dart
+++ lib/workingcomps/drinksdata.dart
@@ -5,84 +5,70 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class Drinksdata extends StatefulWidget {
-   Drinksdata({super.key});
+  Drinksdata({super.key});
 
   @override
   State<Drinksdata> createState() => _DrinksdataState();
 }
 
 class _DrinksdataState extends State<Drinksdata> {
-  List<BeverageModel> drinklist=[]; 
+  List<BeverageModel> drinklist = [];
 
-  Future<List<BeverageModel>> getdrinkapi () async {
-
+  Future<List<BeverageModel>> getdrinkapi() async {
     final response = await http.get(Uri.parse('https://api.sampleapis.com/coffee/hot'));
     var data = jsonDecode(response.body.toString());
-    if(response.statusCode == 200)
-    {
+    if (response.statusCode == 200) {
       drinklist.clear();
-      for(Map i in data)
-      {
+      for (Map i in data) {
         drinklist.add(BeverageModel.fromJson(i));
       }
       return drinklist;
-    }
-    else{
+    } else {
       return drinklist;
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(
-          height: 450,
-          child: FutureBuilder(
-          future: getdrinkapi(), 
-          builder: (context , snapshot)
-          {
-            if(!snapshot.hasData)
-            {
-              return Center(child: CircularProgressIndicator(color: Color.fromARGB(255, 0, 112, 72), ));
-            }
-            else
-            {
-              return Expanded(
-            child: GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-    crossAxisCount: 2, 
-    childAspectRatio: 0.68, 
-  ),
-               
-              itemCount: drinklist.length,
-              itemBuilder: (context, index) {
-                return Container(
-                  margin: EdgeInsets.all(8.0), 
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(20),
-                        child: Image.network(
-                          drinklist[index].image ?? 'https://via.placeholder.com/150',
-                          fit: BoxFit.cover,
-                          width: double.infinity,
-                          
+    return SizedBox(
+      height: 430, 
+      child: FutureBuilder(
+        future: getdrinkapi(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(
+                color: Color.fromARGB(255, 0, 112, 72),
+              ),
+            );
+          } else {
+            return SingleChildScrollView(
+              child: Wrap(
+                spacing: 10.0, 
+                runSpacing: 10.0, 
+                children: List.generate(drinklist.length, (index) {
+                  return Container(
+                    width: MediaQuery.of(context).size.width / 2 - 16, 
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Image.network(
+                            drinklist[index].image ?? 'https://via.placeholder.com/150',
+                            fit: BoxFit.cover,
+                            width: double.infinity,
+                          ),
                         ),
-                      ),
-                      
-                      
-                    ],
-                  ),
-                );
-              },
-            ),
-              );
-            }
-          }),
-        ),
-      ],
-    ) ;
+                      ],
+                    ),
+                  );
+                }),
+              ),
+            );
+          }
+        },
+      ),
+    );
   }
 }
