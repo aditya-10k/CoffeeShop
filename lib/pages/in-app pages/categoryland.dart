6148999ba97d07049552a2f6a2f2dfc7c8804ab6
aditya-10k <katheaditya10@gmail.com>
2024-10-21 23:58:06 +0530

    18th commit

--- lib/pages/in-app pages/categoryland.dart
+++ lib/pages/in-app pages/categoryland.dart
@@ -1,7 +1,9 @@
 import 'package:coffee/workingcomps/apicatcher.dart';
 import 'package:coffee/workingcomps/categorys.dart';
+import 'package:coffee/workingcomps/search.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:icons_plus/icons_plus.dart';
 
 class Categoryland extends StatefulWidget {
   const Categoryland({super.key, required this.category});
@@ -25,48 +27,58 @@ class _CategorylandState extends State<Categoryland> {
           style: GoogleFonts.robotoSlab(
             fontWeight:  FontWeight.bold),),
           centerTitle: true,
+          actions: [
+            IconButton(
+              onPressed: (){ 
+                showSearch(context: context, delegate: SearchUser(widget.category.link));
+              }, 
+              icon: Icon(AntDesign.search_outline))
+          ],
         ),
         backgroundColor: Colors.white,
         body: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.fromLTRB(5, 20, 5, 30),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children:[ FutureBuilder(
-                future: getdrinkapi(widget.category.link),
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData) {
-                    return Center(
-                      child: CircularProgressIndicator(
-                        color: Color.fromARGB(255, 0, 112, 72),
-                      ),
-                    );
-                  } else {
-                    return Center(
-                      child: Wrap(
-                        alignment: WrapAlignment.center,
-                        spacing: 10.0, 
-                        runSpacing: 10.0, 
-                        children: List.generate(drinklist.length, (index) {
-                          return Container(
-                            width: MediaQuery.of(context).size.width / 2 - 16, 
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(20),
-                              child: Image.network(
-                                drinklist[index].image ?? 'https://via.placeholder.com/150',
-                                fit: BoxFit.cover,
-                                height: 300,
-                                width: 200,
+            child: FutureBuilder(
+              future: getdrinkapi(widget.category.link),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(
+                      color: Color.fromARGB(255, 0, 112, 72),
+                    ),
+                  );
+                } else {
+                  return Center(
+                    child: Wrap(
+                      alignment: WrapAlignment.center,
+                      spacing: 20.0, 
+                      runSpacing: 10.0, 
+                      children: List.generate(drinklist.length, (index) {
+                        return Container(
+                          width: MediaQuery.of(context).size.width / 2 - 16, 
+                          child: Column(
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(20),
+                                child: Image.network(
+                                  drinklist[index].image ?? 'https://via.placeholder.com/150',
+                                  fit: BoxFit.cover,
+                                  height: 300,
+                                  width: 200,
+                                ),
                               ),
-                            ),
-                          );
-                        }),
-                      ),
-                    );
-                  }
-                },
-              ),
-              ]
+                              Text(
+                                drinklist[index].name??'no data'
+                                )
+                            ],
+                          ),
+                        );
+                      }),
+                    ),
+                  );
+                }
+              },
             ),
           ),
         ),
---
+++ lib/workingcomps/search.dart
@@ -0,0 +1,99 @@
+import 'package:coffee/workingcomps/apicatcher.dart';
+import 'package:flutter/material.dart';
+import 'package:icons_plus/icons_plus.dart';
+
+class SearchUser extends SearchDelegate{
+
+  final String categorylink;
+  SearchUser(this.categorylink);
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+   return[
+    IconButton(
+      onPressed: (){
+        query = "";
+      },
+      icon: Icon(AntDesign.close_circle_outline),
+      color: Colors.black,
+      )
+   ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: (){Navigator.pop(context);}, 
+      icon: Icon(AntDesign.arrow_left_outline),
+      color: Colors.black,);
+  }
+
+@override
+Widget buildResults(BuildContext context) {
+  List filteredDrinks = drinklist
+      .where((drink) =>
+          drink.name!.toLowerCase().contains(query.toLowerCase()))
+      .toList();
+
+  return Scaffold(
+    backgroundColor: Colors.white,
+    body: SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(5, 20, 5, 30),
+        child: FutureBuilder(
+          future: getdrinkapi(categorylink), // Still fetching the API data
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Center(
+                child: CircularProgressIndicator(
+                  color: Color.fromARGB(255, 0, 112, 72),
+                ),
+              );
+            } else {
+              return filteredDrinks.isEmpty
+                  ? Center(child: Text('No Results Found'))
+                  : Center(
+                      child: Wrap(
+                        alignment: WrapAlignment.center,
+                        spacing: 20.0,
+                        runSpacing: 10.0,
+                        children: List.generate(filteredDrinks.length, (index) {
+                          return Container(
+                            width: MediaQuery.of(context).size.width / 2 - 16,
+                            child: Column(
+                              children: [
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(20),
+                                  child: Image.network(
+                                    filteredDrinks[index].image ??
+                                        'https://via.placeholder.com/150',
+                                    fit: BoxFit.cover,
+                                    height: 300,
+                                    width: 200,
+                                  ),
+                                ),
+                                Text(
+                                  filteredDrinks[index].name ?? 'No data',
+                                ),
+                              ],
+                            ),
+                          );
+                        }),
+                      ),
+                    );
+            }
+          },
+        ),
+      ),
+    ),
+  );
+}
+
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return Center(child: Text('Search your mind'));
+  }
+
+  
+}
